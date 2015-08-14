cb14bb1f5063e3f972aa6da4be7dcc7bd6d42ce4
Kevin-Leong <kevin.leong.cn@gmail.com>
2015-08-14 12:28:02 +0800

    8

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,6 @@
 module UsersHelper
-  # 􏷲􏷳􏷴􏷮􏱸􏱹􏱺 Gravatar 
+  
+  # Returns the Gravatar for the given user.
   def gravatar_for(user)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
