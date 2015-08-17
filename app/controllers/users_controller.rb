5caad5057b0831e04b66d15308f94022f9c716c2
Kevin-Leong <kevin.leong.cn@gmail.com>
2015-08-17 20:00:34 +0800

    login/logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,13 @@
 class UsersController < ApplicationController
+  
   def show
     @user = User.find(params[:id])
   end
-
+  
   def new
     @user = User.new
   end
-
+  
   def create
     @user = User.new(user_params)
     if @user.save
@@ -14,7 +15,7 @@ class UsersController < ApplicationController
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
-      render 'new' 
+      render 'new'
     end
   end
   
